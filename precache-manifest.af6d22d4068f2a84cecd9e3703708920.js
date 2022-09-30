2926ef6332f029132abfecfd9cb807a64ee89816
DipamAmin <DipamAmin@users.noreply.github.com>
2022-09-30 10:46:56 +0000

    deploy: 11a2046d0e0b9a28176351ce6e001a198e9eb445

---
+++ precache-manifest.af6d22d4068f2a84cecd9e3703708920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43f7dad8868b11cf8be6d06efcec8be",
+    "url": "https://DipamAmin.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "89e225a9071af6c6cee9",
+    "url": "https://DipamAmin.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "e49e8f788c99fa3f4c59",
+    "url": "https://DipamAmin.github.io/task-management-frontend/static/js/2.c4bcef73.chunk.js"
+  },
+  {
+    "revision": "89e225a9071af6c6cee9",
+    "url": "https://DipamAmin.github.io/task-management-frontend/static/js/main.7d67a7b6.chunk.js"
+  },
+  {
+    "revision": "972d0fc250e6b655965d",
+    "url": "https://DipamAmin.github.io/task-management-frontend/static/js/runtime~main.f2e4abd1.js"
+  }
+]);
